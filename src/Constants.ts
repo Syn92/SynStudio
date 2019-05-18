2f6368bfd1443c084d5bc6f43f522d05d4973737
Syn92 <35808535+Syn92@users.noreply.github.com>
2019-05-18 14:50:56 -0400

    routing

---
+++ src/Constants.ts
@@ -0,0 +1,10 @@
+export class Constants {
+    // public static readonly
+    public static readonly appName: string = 'SynStudio';
+    public static readonly about: string = 'À propos';
+    public static readonly contact: string = 'Contact';
+    public static readonly product: string = 'Produit';
+    public static readonly ABOUT_PATH: string = 'about';
+    public static readonly CONTACT_PATH: string = 'contact';
+    public static readonly PRODUCT_PATH: string = 'product';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,50 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainViewComponent } from './main-view/main-view.component';
+import { Constants } from 'Constants';
+import { AboutViewComponent } from './about-view/about-view.component';
+import { ContactViewComponent } from './contact-view/contact-view.component';
+
+const routes: Routes = [
+  { path: '', component: MainViewComponent,
+  children: [
+    {
+      path: Constants.ABOUT_PATH, component: AboutViewComponent,
+    },
+    {
+      path: Constants.CONTACT_PATH, component: ContactViewComponent,
+    }
+  ]},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    AboutViewComponent,
+    ContactViewComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     FormsModule,
     HttpModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
+  exports: [RouterOutlet],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
